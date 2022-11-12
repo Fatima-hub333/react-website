7b10a08cac3a52485a20998a0a5919b4645f5c2f
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-12 14:40:32 +0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import {Article, Brand, CTA, Feature, Navbar} from './components'
+import {Brand, CTA, Navbar} from './components'
+import {Blog, Features, Footer, Header, Possibility, WhatGPT3} from './containers'
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT- 3</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './posibility.css'
+import './possibility.css'
 
 const Possibility = () => {
   return (
