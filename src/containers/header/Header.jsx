d0956f5c01fe054d2e83ad096b41f8a189caf4cc
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-14 22:24:26 +0500

    Start header section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,16 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Let's Build Something amazing with GPPT-3 OpenAI</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <div className='gpt3__header-content__input'>
+          <input type="email" placeholder='Your Email Address' />
+          <button type="button">Get Started</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
