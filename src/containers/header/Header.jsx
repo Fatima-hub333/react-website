1026f09be94d834602e827d8390f1e260203fde8
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-15 16:32:40 +0500

    Header section content & styling done

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './header.css'
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
@@ -11,6 +13,13 @@ const Header = () => {
           <input type="email" placeholder='Your Email Address' />
           <button type="button">Get Started</button>
         </div>
+        <div className='gpt3__header-content__people' >
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className='gpt3__header-content-image'>
+        <img src={ai} alt="ai" />
       </div>
     </div>
   )
