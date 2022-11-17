54a4c029d126a73c1b8ed7418cd933ca6b1cd885
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-17 11:46:00 +0500

    Fix bugs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import {Brand, CTA, Navbar} from './components'
-import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers'
-import './App.css'
+import React from 'react';
 
-const App = () => {
-  return (
-    <div className='App'>
-      <div className='gradient__bg'>
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  )
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './possibility.css'
+import possibilityImage from '../../assets/possibility.png'
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='gpt3__possibility section__padding' id="possibility">
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+    </div>
   )
 }
 
