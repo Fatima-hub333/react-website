d7ac56dc63407f67180a9a000ee466a581eeb155
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-17 15:30:11 +0500

    Done with blog content & styling

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './blog.css'
+import { Article } from '../../components'
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding' id="blog">
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+        </div>
+          <Article imgUrl={blog01} date="Sep 26, 2022" title="Open AI is the future. Let us explore how it is?"/>
+        <div className='gpt3__blog-container_groupB'>
+          <Article imgUrl={blog02} date="Sep 26, 2022" title="Open AI is the future. Let us explore how it is?"/>
+          <Article imgUrl={blog03} date="Sep 26, 2022" title="Open AI is the future. Let us explore how it is?"/>
+          <Article imgUrl={blog04} date="Sep 26, 2022" title="Open AI is the future. Let us explore how it is?"/>
+          <Article imgUrl={blog05} date="Sep 26, 2022" title="Open AI is the future. Let us explore how it is?"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
